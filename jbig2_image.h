587ce20bd523d6806893c076717bc98c0d73058e
cvs2svn <cvs2svn@ded80894-8fb9-0310-811b-c03f3676ab4d>
2003-03-23 20:37:19 +0000

    1 copies to tags/branches
    git-svn-id: http://svn.ghostscript.com/jbig2dec/tags/release_0_01@73 ded80894-8fb9-0310-811b-c03f3676ab4d

---
+++ jbig2_image.h
@@ -0,0 +1,31 @@
+/*
+    jbig2dec
+    
+    Copyright (c) 2001-2002 artofcode LLC.
+    
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    $Id: jbig2_image.h,v 1.5 2002/06/18 13:40:29 giles Exp $
+*/
+
+
+#ifndef _JBIG2_IMAGE_H
+#define _JBIG2_IMAGE_H
+
+/* routines for dumping the image data in various formats */
+/* FIXME: should these be in the client instead? */
+
+int jbig2_image_write_pbm_file(Jbig2Image *image, char *filename);
+int jbig2_image_write_pbm(Jbig2Image *image, FILE *out);
+Jbig2Image *jbig2_image_read_pbm_file(Jbig2Ctx *ctx, char *filename);
+Jbig2Image *jbig2_image_read_pbm(Jbig2Ctx *ctx, FILE *in);
+
+#ifdef HAVE_LIBPNG
+int jbig2_image_write_png_file(Jbig2Image *image, char *filename);
+int jbig2_image_write_png(Jbig2Image *image, FILE *out);
+#endif
+
+#endif /* _JBIG2_IMAGE_H */
